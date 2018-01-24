fb81f850979b48d890c879af96fcf00ad55e02cd
James Smith <jecs@imperial.ac.uk>
2018-01-24 23:18:21 +0000

    A small change to a message.

--- es6/messages.js
+++ es6/messages.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const serverFailedToRespondMessage = 'The server failed to respond.',
+const serverFailedToRespondMessage = 'The server failed to respond in a timely fasion.',
       invalidUsernameMessage = 'Usernames must consist of groups of at least two and no more than sixteen numbers or lowercase letters, separated by dashes.',
       invalidPasswordMessage = 'Passwords must consist of at least eight letters, numbers or common punctuation symbols.',
       invalidEmailAddressMessage = 'The email address does not appear to be a valid one.',
       invalidConfirmationCodeMessage = 'Confirmation codes consist of eight alphabetical characters.',
       passwordsDoNoMatchMessage = 'The passwords do not match.',
-      failedToLoginMessage = 'Failed to login. The message from the server is:',
-      failedToRegisterMessage = 'Failed to register. The message from the server is:'
+      failedToLoginMessage = 'Failed to login. The message from the server was...',
+      failedToRegisterMessage = 'Failed to register. The message from the server was...'
 
 const messages = {
   serverFailedToRespondMessage: serverFailedToRespondMessage, 
