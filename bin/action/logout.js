eaae3417df28b07db6781cca826e849ab8414367
James Smith <james.smith@sapient.com>
2018-02-15 15:14:14 +0000

    Added the logging out functionality.

--- bin/action/logout.js
+++ bin/action/logout.js
@@ -1,14 +1,34 @@
 'use strict';
 
+const necessary = require('necessary')
+
 const action = require('../action'),
+      messages = require('../messages'),
       constants = require('../constants');
 
-function logout() {
-  const callbacks = [],
-        context = {},
-        uri = 'logout';
+const { miscellaneousUtilities } = necessary,
+      { rc } = miscellaneousUtilities,
+      { readRCFile, updateRCFile } = rc,
+      { LOGOUT_URI } = constants,
+      { LOGGED_OUT_MESSAGE } = messages;
 
-  action(callbacks, context, uri);
+function logout() {
+  const json = readRCFile(),
+        { accessToken } = json;
+
+  if (accessToken) {
+    const callbacks = [],
+          context = {
+            accessToken: accessToken
+          },
+          uri = LOGOUT_URI;
+    
+    action(callbacks, context, uri, function(json) {
+      console.log(LOGGED_OUT_MESSAGE);
+    });
+  } else {
+    console.log(LOGGED_OUT_MESSAGE);
+  }
 }
 
 module.exports = logout;
