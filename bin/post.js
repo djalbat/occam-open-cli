d11432527b734461a5b07d096aec9c3698994674
James Smith <jecs@imperial.ac.uk>
2018-05-11 20:41:15 +0100

    Improved the error handling around parsing the JSON server response.

--- bin/post.js
+++ bin/post.js
@@ -44,26 +44,31 @@ function post(uri, data, callback) {
     if (error) {
       console.log(SERVER_FAILED_TO_RESPOND_ERROR_MESSAGE);
     } else {
+      const { body } = response;
+
+      let json;
+
       try {
-        const { body } = response,
-              json = JSON.parse(body);
-
-        const { info, message, error } = json;
-
-        if (info) { ///
-          console.log(info);
-        }
-
-        if (message) {  ///
-          console.log(message);
-        }
-
-        error ?
-          console.log(SERVER_ERROR_MESSAGE) :
-            callback(json);
+        json = JSON.parse(body);
       } catch (error) {
         console.log(SERVER_ERROR_MESSAGE);
+
+        exit();
       }
+
+      const { info, message, error } = json;
+
+      if (info) { ///
+        console.log(info);
+      }
+
+      if (message) {  ///
+        console.log(message);
+      }
+
+      error ?
+        console.log(SERVER_ERROR_MESSAGE) :
+          callback(json);
     }
 
     exit();
