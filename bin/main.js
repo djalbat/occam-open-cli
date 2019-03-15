8fe4266d62355949685a6f47df35bdfbf6d29bb0
James Smith <james.smith@openmathematics.org>
2019-03-15 08:53:04 +0000

    And again.

--- bin/main.js
+++ bin/main.js
@@ -7,9 +7,9 @@ const actions = require('./actions'),
 			directoryUtilities = require('./utilities/directory');
 
 const { exit } = process,
+      { PUBLISH_COMMAND } = commands,
 			{ changeDirectory } = directoryUtilities,
       { FAILED_PUBLISH_NO_CONFIGURATION_FILE_MESSAGE } = messages,
-      { HELP_COMMAND, VERSION_COMMAND, PUBLISH_COMMAND } = commands,
       { checkConfigurationFileExists, upgradeConfigurationFile, createConfigurationFile } = configuration;
 
 function main(command, argument, options) {
