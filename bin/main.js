ac9f9fdc791b0435c152096785f5b74f6eaa2102
James Smith <james.smith@openmathematics.org>
2019-03-15 08:52:42 +0000

    Tiny changes.

--- bin/main.js
+++ bin/main.js
@@ -7,9 +7,9 @@ const actions = require('./actions'),
 			directoryUtilities = require('./utilities/directory');
 
 const { exit } = process,
-      { PUBLISH_COMMAND } = commands,
 			{ changeDirectory } = directoryUtilities,
       { FAILED_PUBLISH_NO_CONFIGURATION_FILE_MESSAGE } = messages,
+      { HELP_COMMAND, VERSION_COMMAND, PUBLISH_COMMAND } = commands,
       { checkConfigurationFileExists, upgradeConfigurationFile, createConfigurationFile } = configuration;
 
 function main(command, argument, options) {
@@ -18,9 +18,7 @@ function main(command, argument, options) {
   if (configurationFileExists) {
     upgradeConfigurationFile();
   } else {
-    const commandPublishCommand = (command === PUBLISH_COMMAND);
-
-    if (commandPublishCommand) {
+    if (command === PUBLISH_COMMAND) {
       const releaseName = changeDirectory();
 
       if (releaseName !== null) {
