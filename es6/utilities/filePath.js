bf380188b8e2de89f91b08bd2fc7f196f619f4d5
James Smith <james.smith@openmathematics.org>
2019-03-18 09:58:14 +0000

    Simplified the file path utilities.

--- es6/utilities/filePath.js
+++ es6/utilities/filePath.js
@@ -2,31 +2,17 @@
 
 const readmeFilePathPattern = '^(?:[^\\/]+\\/){1}README\\.md$',
       florenceFilePathPattern = '^(?:[^\\/]+\\/){1,}[^\\.]+\\.ocs$',
-      validMetaJSONFilePathPattern = '^(?:[^\\/]+\\/){1}meta\\.json$',
-      invalidMetaJSONFilePathPattern = '^(?:[^\\/]+\\/){2,}meta\\.json$',
-      validCustomGrammarBNFFilePathPattern = '^(?:[^\\/]+\\/){1}(term|expression|statement|metastatement)\\.bnf$',
-      invalidCustomGrammarBNFFilePathPattern = '^(?:[^\\/]+\\/){2,}(term|expression|statement|metastatement)\\.bnf$',
-      validCustomGrammarLexicalPatternFilePathPattern = '^(?:[^\\/]+\\/){1}pattern\\.lex$',
-      invalidCustomGrammarLexicalPatternFilePathPattern = '^(?:[^\\/]+\\/){2,}pattern\\.lex$',
-      metaJSONFilePathPattern = validMetaJSONFilePathPattern, ///
-      customGrammarBNFFilePathPattern = validCustomGrammarBNFFilePathPattern, ///
-      customGrammarLexicalPatternFilePathPattern = validCustomGrammarLexicalPatternFilePathPattern, ///
-      validFilePathPattern = `${validMetaJSONFilePathPattern}|${validCustomGrammarBNFFilePathPattern}|${validCustomGrammarLexicalPatternFilePathPattern}`,
-      invalidFilePathPattern = `${invalidMetaJSONFilePathPattern}|${invalidCustomGrammarBNFFilePathPattern}|${invalidCustomGrammarLexicalPatternFilePathPattern}`,
+      metaJSONFilePathPattern = '^(?:[^\\/]+\\/){1}meta\\.json$',
+      customGrammarBNFFilePathPattern = '^(?:[^\\/]+\\/){1}(term|expression|statement|metastatement)\\.bnf$',
+      customGrammarLexicalPatternFilePathPattern = '^(?:[^\\/]+\\/){1}pattern\\.lex$',
       recognisedFilePathPattern = `${readmeFilePathPattern}|${florenceFilePathPattern}|${metaJSONFilePathPattern}|${customGrammarBNFFilePathPattern}|${customGrammarLexicalPatternFilePathPattern}`;
 
 const readmeFilePathRegularExpression = new RegExp(readmeFilePathPattern),
       florenceFilePathRegularExpression = new RegExp(florenceFilePathPattern),
       metaJSONFilePathRegularExpression = new RegExp(metaJSONFilePathPattern),
-      validFilePathRegularExpression = new RegExp(validFilePathPattern),
-      invalidFilePathRegularExpression = new RegExp(invalidFilePathPattern),
       recognisedFilePathRegularExpression = new RegExp(recognisedFilePathPattern),
-      validMetaJSONFilePathRegularExpression = new RegExp(validMetaJSONFilePathPattern),
-      invalidMetaJSONFilePathRegularExpression = new RegExp(invalidMetaJSONFilePathPattern),
-      validCustomGrammarBNFFilePathRegularExpression = new RegExp(validCustomGrammarBNFFilePathPattern),
-      invalidCustomGrammarBNFFilePathRegularExpression = new RegExp(invalidCustomGrammarBNFFilePathPattern),
-      validCustomGrammarLexicalPatternFilePathRegularExpression = new RegExp(validCustomGrammarLexicalPatternFilePathPattern),
-      invalidCustomGrammarLexicalPatternFilePathRegularExpression = new RegExp(invalidCustomGrammarLexicalPatternFilePathPattern);
+      customGrammarBNFFilePathRegularExpression = new RegExp(customGrammarBNFFilePathPattern),
+      customGrammarLexicalPatternFilePathRegularExpression = new RegExp(customGrammarLexicalPatternFilePathPattern);
 
 function isFilePathReadmeFilePath(filePath) { return readmeFilePathRegularExpression.test(filePath); }
 
@@ -34,35 +20,17 @@ function isFilePathFlorenceFilePath(filePath) { return florenceFilePathRegularEx
 
 function isFilePathMetaJSONFilePath(filePath) { return metaJSONFilePathRegularExpression.test(filePath); }
 
-function isFilePathValidFilePath(filePath) { return validFilePathRegularExpression.test(filePath); }
-
-function isFilePathInvalidFilePath(filePath) { return invalidFilePathRegularExpression.test(filePath); }
-
 function isFilePathRecognisedFilePath(filePath) { return recognisedFilePathRegularExpression.test(filePath); }
 
-function isFilePathValidMetaJSONFilePath(filePath) { return validMetaJSONFilePathRegularExpression.test(filePath); }
+function isFilePathCustomGrammarBNFFilePath(filePath) { return customGrammarBNFFilePathRegularExpression.test(filePath); }
 
-function isFilePathInvalidMetaJSONFilePath(filePath) { return invalidMetaJSONFilePathRegularExpression.test(filePath); }
-
-function isFilePathValidCustomGrammarBNFFilePath(filePath) { return validCustomGrammarBNFFilePathRegularExpression.test(filePath); }
-
-function isFilePathInvalidCustomGrammarBNFFilePath(filePath) { return invalidCustomGrammarBNFFilePathRegularExpression.test(filePath); }
-
-function isFilePathValidCustomGrammarLexicalPatternFilePath(filePath) { return validCustomGrammarLexicalPatternFilePathRegularExpression.test(filePath); }
-
-function isFilePathInvalidCustomGrammarLexicalPatternFilePath(filePath) { return invalidCustomGrammarLexicalPatternFilePathRegularExpression.test(filePath); }
+function isFilePathCustomGrammarLexicalPatternFilePath(filePath) { return customGrammarLexicalPatternFilePathRegularExpression.test(filePath); }
 
 module.exports = {
   isFilePathReadmeFilePath,
   isFilePathFlorenceFilePath,
   isFilePathMetaJSONFilePath,
-  isFilePathValidFilePath,
-  isFilePathInvalidFilePath,
   isFilePathRecognisedFilePath,
-  isFilePathValidMetaJSONFilePath,
-  isFilePathInvalidMetaJSONFilePath,
-  isFilePathValidCustomGrammarBNFFilePath,
-  isFilePathInvalidCustomGrammarBNFFilePath,
-  isFilePathValidCustomGrammarLexicalPatternFilePath,
-  isFilePathInvalidCustomGrammarLexicalPatternFilePath
+  isFilePathCustomGrammarBNFFilePath,
+  isFilePathCustomGrammarLexicalPatternFilePath
 };
