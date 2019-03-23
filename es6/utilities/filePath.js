0cd7e3ca0e84edcc75e1adf1892a41c2a978ba54
James Smith <james.smith@openmathematics.org>
2019-03-23 11:59:19 +0000

    Added to the file path utilities.

--- es6/utilities/filePath.js
+++ es6/utilities/filePath.js
@@ -5,12 +5,14 @@ const readmeFilePathPattern = '^(?:[^\\/]+\\/){1}README\\.md$',
       metaJSONFilePathPattern = '^(?:[^\\/]+\\/){1}meta\\.json$',
       customGrammarBNFFilePathPattern = '^(?:[^\\/]+\\/){1}(term|expression|statement|metastatement)\\.bnf$',
       customGrammarLexicalPatternFilePathPattern = '^(?:[^\\/]+\\/){1}pattern\\.lex$',
+      customGrammarFilePathPattern = `${customGrammarBNFFilePathPattern}|${customGrammarLexicalPatternFilePathPattern}`,
       recognisedFilePathPattern = `${readmeFilePathPattern}|${florenceFilePathPattern}|${metaJSONFilePathPattern}|${customGrammarBNFFilePathPattern}|${customGrammarLexicalPatternFilePathPattern}`;
 
 const readmeFilePathRegularExpression = new RegExp(readmeFilePathPattern),
       florenceFilePathRegularExpression = new RegExp(florenceFilePathPattern),
       metaJSONFilePathRegularExpression = new RegExp(metaJSONFilePathPattern),
       recognisedFilePathRegularExpression = new RegExp(recognisedFilePathPattern),
+      customGrammarFilePathRegularExpression = new RegExp(customGrammarFilePathPattern),
       customGrammarBNFFilePathRegularExpression = new RegExp(customGrammarBNFFilePathPattern),
       customGrammarLexicalPatternFilePathRegularExpression = new RegExp(customGrammarLexicalPatternFilePathPattern);
 
@@ -22,6 +24,8 @@ function isFilePathMetaJSONFilePath(filePath) { return metaJSONFilePathRegularEx
 
 function isFilePathRecognisedFilePath(filePath) { return recognisedFilePathRegularExpression.test(filePath); }
 
+function isFilePathCustomGrammarFilePath(filePath) { return customGrammarFilePathRegularExpression.test(filePath); }
+
 function isFilePathCustomGrammarBNFFilePath(filePath) { return customGrammarBNFFilePathRegularExpression.test(filePath); }
 
 function isFilePathCustomGrammarLexicalPatternFilePath(filePath) { return customGrammarLexicalPatternFilePathRegularExpression.test(filePath); }
@@ -31,6 +35,7 @@ module.exports = {
   isFilePathFlorenceFilePath,
   isFilePathMetaJSONFilePath,
   isFilePathRecognisedFilePath,
+  isFilePathCustomGrammarFilePath,
   isFilePathCustomGrammarBNFFilePath,
   isFilePathCustomGrammarLexicalPatternFilePath
 };
