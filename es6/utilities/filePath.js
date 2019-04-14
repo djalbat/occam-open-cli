601444db46e677c2b2e28f19f08e15d79c9d25b0
James Smith <james.smith@openmathematics.org>
2019-04-14 09:33:31 +0100

    Changed the Florence file path and bumped the major version number.

--- es6/utilities/filePath.js
+++ es6/utilities/filePath.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const readmeFilePathPattern = '^(?:[^\\/]+\\/){1}README\\.md$',
-      florenceFilePathPattern = '^(?:[^\\/]+\\/){1,}[^\\.]+\\.ocs$',
+      florenceFilePathPattern = '^(?:[^\\/]+\\/){1,}[^\\.]+\\.fls$',
       metaJSONFilePathPattern = '^(?:[^\\/]+\\/){1}meta\\.json$',
       customGrammarBNFFilePathPattern = '^(?:[^\\/]+\\/){1}(term|expression|statement|metastatement)\\.bnf$',
       customGrammarLexicalPatternFilePathPattern = '^(?:[^\\/]+\\/){1}pattern\\.lex$',
