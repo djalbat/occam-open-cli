deacd5e722d34bd4d7403059e93e9fe6a9153382
James Smith <james.smith@djalbat.com>
2021-08-10 19:11:16 +0100

    Put the semi-colons in the index files.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 'use strict';
 
-export { default as File } from "./file"
-export { default as Files } from "./files"
-export { default as Release } from "./release"
-export { default as Entries } from "./entries"
-export { default as Project } from "./project"
-export { default as Projects } from "./projects"
+export { default as File } from "./file";
+export { default as Files } from "./files";
+export { default as Release } from "./release";
+export { default as Entries } from "./entries";
+export { default as Project } from "./project";
+export { default as Projects } from "./projects";
 
-export { default as Version } from "./version"
-export { default as filePathUtilities } from "./utilities/filePath"
+export { default as Version } from "./version";
+export { default as filePathUtilities } from "./utilities/filePath";
 
-export { default as fileSystemUtilities } from "./utilities/fileSystem"
+export { default as fileSystemUtilities } from "./utilities/fileSystem";
