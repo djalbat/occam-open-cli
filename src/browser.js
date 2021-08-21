5016098e226b45241f6fef3062447b1d1ac29be2
James Smith <james.smith@djalbat.com>
2021-08-21 09:53:08 +0100

    Cosmetic changes.

--- src/browser.js
+++ src/browser.js
@@ -2,10 +2,9 @@
 
 export { default as File } from "./file";
 export { default as Files } from "./files";
+export { default as Version } from "./version";
 export { default as Release } from "./release";
 export { default as Entries } from "./entries";
 export { default as Project } from "./project";
 export { default as Projects } from "./projects";
-
-export { default as Version } from "./version";
 export { default as filePathUtilities } from "./utilities/filePath";
--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@
 
 export { default as File } from "./file";
 export { default as Files } from "./files";
+export { default as Version } from "./version";
 export { default as Release } from "./release";
 export { default as Entries } from "./entries";
 export { default as Project } from "./project";
 export { default as Projects } from "./projects";
-
-export { default as Version } from "./version";
 export { default as filePathUtilities } from "./utilities/filePath";
-
 export { default as fileSystemUtilities } from "./utilities/fileSystem";
