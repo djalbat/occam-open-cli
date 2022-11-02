bac8fe6ab206cd17f7559b26494783ce110324a9
James Smith <james.smith@djalbat.com>
2022-11-02 19:05:21 +0000

    Propagated.

--- bin/operation/updateVersion.js
+++ bin/operation/updateVersion.js
@@ -1,18 +1,27 @@
 "use strict";
 
-const { fileSystemUtilities } = require("necessary");
+const { fileNames, metaJSONUtilities, fileSystemUtilities } = require("occam-file-system");
 
-const { fileNames } = require("occam-file-system");
+const { PERIOD } = require("../constants");
 
-const { META_JSON_FILE_NAME } = fileNames,
+const { loadFile, saveFile } = fileSystemUtilities,
+      { META_JSON_FILE_NAME } = fileNames,
+      { repositoryFromNode, dependenciesFromNode, metaJSONNodeFromMetaJSONFile } = metaJSONUtilities;
 
 function updateVersionOperation(proceed, abort, context) {
   const { success } = context;
 
   if (success) {
-    const { version } = context;
+    const { version } = context,
+          path = META_JSON_FILE_NAME, ///
+          projectsDirectoryPath = PERIOD,
+          metaJSONFile = loadFile(path, projectsDirectoryPath),
+          metaJSONNode = metaJSONNodeFromMetaJSONFile(metaJSONFile),
+          node = metaJSONNode,  ///
+          repository = repositoryFromNode(node),
+          dependencies = dependenciesFromNode(node);
 
-
+    debugger
   }
 
   proceed();
