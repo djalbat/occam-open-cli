b7e9e731ac0094643ca7ac9025f993c32c9ee149
James Smith <james.smith@djalbat.com>
2023-08-15 19:08:50 +0100

    Improved the version message.

--- bin/action/version.js
+++ bin/action/version.js
@@ -1,13 +1,13 @@
 "use strict";
 
-const { OPEN_CLI } = require("../constants"),
+const { OCCAM_OPEN_CLI } = require("../constants"),
       { getPackageVersion } = require("../utilities/packageJSON");
 
 function versionAction() {
   const packageVersion = getPackageVersion(),
         version = packageVersion; ///
 
-  console.log(`${OPEN_CLI} version ${version}`);
+  console.log(`${OCCAM_OPEN_CLI} version ${version}`);
 
   process.exit();
 }
--- bin/constants.js
+++ bin/constants.js
@@ -6,12 +6,12 @@ const NO = "no",
       DATA = "data",
       OPEN = "open",
       HOST_URL = "hostURL",
-      OPEN_CLI = "Open-CLI",
       DOUBLE_DOTS = "..",
       DOUBLE_DASH = "--",
       EMPTY_STRING = "",
       RELEASE_JSON = "release.json",
-      PACKAGE_JSON = "package.json";
+      PACKAGE_JSON = "package.json",
+      OCCAM_OPEN_CLI = "Occam Open CLI";
 
 module.exports = {
   NO,
@@ -20,10 +20,10 @@ module.exports = {
   DATA,
   OPEN,
   HOST_URL,
-  OPEN_CLI,
   DOUBLE_DOTS,
   DOUBLE_DASH,
   EMPTY_STRING,
   RELEASE_JSON,
-  PACKAGE_JSON
+  PACKAGE_JSON,
+  OCCAM_OPEN_CLI
 };
